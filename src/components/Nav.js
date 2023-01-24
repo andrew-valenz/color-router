31fa915015fcdf6738bb1dbd9f71ac0e58b03bc4
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-24 10:19:55 -0800

    added functions to Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import 'Nav.css';
+
+export async function Green() {
+  return (
+    <>
+      <h2>Green</h2>
+    </>
+  );
+}
+
+export async function Blue() {
+  return (
+    <>
+      <h2>Blue</h2>
+    </>
+  );
+}
+export async function Red() {
+  return (
+    <>
+      <h2>Red</h2>
+    </>
+  );
+}
